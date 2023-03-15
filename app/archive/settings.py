c89fd611810dbe4d83993cf1dd9c2affdcb0f0fc
root <5903811+kavonjon@users.noreply.github.com>
2023-03-15 12:50:58 +0000

    fix email settings

--- app/archive/settings.py
+++ app/archive/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import ast
 #from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -143,6 +144,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-#EMAIL_HOST_USER = secrets
-#EMAIL_HOST_PASSWORD = secrets
-#ADMINS = secrets
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+ADMINS = ast.literal_eval(os.getenv('ADMINS'))

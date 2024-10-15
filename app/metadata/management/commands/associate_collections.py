8a9351c97cac009e7993491d0a910cc1473df933
kavon <5903811+kavonjon@users.noreply.github.com>
2024-10-15 18:20:04 +0400

    adjust command to associate items with collections

--- app/metadata/management/commands/associate_collections.py
+++ app/metadata/management/commands/associate_collections.py
@@ -2,14 +2,12 @@ import re, copy
 from openpyxl import Workbook, load_workbook
 from django.db.models import Count, Sum, Max, Q
 from django.db import transaction
-from metadata.models import Item, ItemTitle, Language, Dialect, DialectInstance, Collaborator, CollaboratorRole, Geographic, Columns_export, Document, Video, ACCESS_CHOICES, ACCESSION_CHOICES, AVAILABILITY_CHOICES, CONDITION_CHOICES, CONTENT_CHOICES, FORMAT_CHOICES, GENRE_CHOICES, MONTH_CHOICES, ROLE_CHOICES, LANGUAGE_DESCRIPTION_CHOICES, reverse_lookup_choices, validate_date_text
+from metadata.models import Item, ItemTitle, Language, Dialect, DialectInstance, Collaborator, CollaboratorRole, Geographic, Columns_export, Document, Video, ACCESS_CHOICES, ACCESSION_CHOICES, AVAILABILITY_CHOICES, CONDITION_CHOICES, CONTENT_CHOICES, FORMAT_CHOICES, GENRE_CHOICES, MONTH_CHOICES, ROLE_CHOICES, LANGUAGE_DESCRIPTION_CHOICES, reverse_lookup_choices, validate_date_text, Collection
 from metadata.views import is_valid_param
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, **options):
-
-        from metadata.models import Collection, Item
 
         # Build the collections map
         collections_map = {
@@ -40,9 +38,16 @@ class Command(BaseCommand):
 
         for item in Item.objects.all():
             if item.collection_name and item.collection_name in collections_map:
-                item.collection_abbr = collections_map[item.collection_name]
-                item.save()
-                print(f"Updated item {item.id} with collection_abbr {item.collection_abbr}")
+                collection_abbr = collections_map[item.collection_name]
+                try:
+                    collection = Collection.objects.get(collection_abbr=collection_abbr)
+                    item.collection = collection
+                    item.save()
+                except Collection.DoesNotExist:
+                    print(f"No Collection found with abbreviation: {collection_abbr} for item: {item.id}")
+            else:
+                print(f"No matching collection found for item: {item.id} with collection_name: {item.collection_name}")
+            print(f"Updated item {item.id} with collection_abbr {item.collection_abbr}")
+            input()
 
         print("All items updated.")
-
